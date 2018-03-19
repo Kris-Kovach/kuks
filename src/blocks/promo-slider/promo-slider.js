482abd1eb0e92f9a6d6f0c810d4fe12a1209f0c8
Kristina Kovach <Goncharenkokn@gmail.com>
2018-03-20 00:16:06 +0300

    Add changing of header bg

--- src/blocks/promo-slider/promo-slider.js
+++ src/blocks/promo-slider/promo-slider.js
@@ -1,8 +1,35 @@
 $(document).ready(function () {
-  $('.promo-slider').owlCarousel({
+  var promoSlider = $('.promo-slider');
+  var headerTopBlock = $('#header-top');
+  var slide;
+  // var color;
+
+  var setHeaderBackground = function (color) {
+    headerTopBlock.css('background-color', color);
+  };
+
+  promoSlider.owlCarousel({
     items: 1,
     nav: false,
     dots: true,
-    loop: true
+    loop: false,
+    mouseDrag: false,
+    touchDrag: false,
+    animateOut: 'fadeOut',
+    dotsSpeed: 1000
   });
+
+  promoSlider.on('changed.owl.carousel',function(property){
+    var currentIndex = property.item.index;
+    var color = $(property.target).find('.owl-item').eq(currentIndex).find('.promo-slider__item').data('color');
+    setHeaderBackground(color);
+  });
+
+  // slide = $('.owl-item.active .promo-slider__item');
+  // setHeaderBackground(slide.data('color'));
+  //
+  // promoSlider.on('translated.owl.carousel', function() {
+  //   slide = $('.owl-item.active .promo-slider__item');
+  //   setHeaderBackground(slide.data('color'));
+  // })
 });
