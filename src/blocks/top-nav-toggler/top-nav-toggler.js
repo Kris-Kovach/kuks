49d72f8fe07304a0a18d315c75c738c87661c704
Kristina Kovach <Goncharenkokn@gmail.com>
2018-03-24 21:57:27 +0300

    Add main nav functionality

--- src/blocks/top-nav-toggler/top-nav-toggler.js
+++ src/blocks/top-nav-toggler/top-nav-toggler.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
+  var mainNavToggler = $('.main-nav-toggler');
   var topNavToggler = $('.top-nav-toggler');
   var topNav = $('.top-nav');
 
-  topNavToggler.on('click', function () {
+  topNavToggler.on('click', function (evt) {
+    evt.preventDefault();
     topNavToggler.toggleClass('top-nav-toggler--close');
     topNav.toggleClass('top-nav--visible');
+    mainNavToggler.toggleClass('main-nav-toggler--disabled');
   });
 });
