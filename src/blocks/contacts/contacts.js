80c19af970fa0bdc8e872b6bf5b7c1ec7afb583f
Kristina Kovach <Goncharenkokn@gmail.com>
2018-05-19 01:09:12 +0300

    Add dat attributes for map

--- src/blocks/contacts/contacts.js
+++ src/blocks/contacts/contacts.js
@@ -1,17 +1,24 @@
 $(document).ready(function () {
 
   if ($('#contacts-map').length) {
+
+    var mapContainer = $('#contacts-map');
+    var lon = mapContainer.data('lon');
+    var lat = mapContainer.data('lat');
+    var address = mapContainer.data('address');
+    var zoom = mapContainer.data('zoom');
+
     ymaps.ready(function () {
       var contactsMap = new ymaps.Map('contacts-map', {
-          center: [59.938196, 30.364809],
-          zoom: 16
+          center: [lon, lat],
+          zoom: zoom
         }),
         kuks = new ymaps.Placemark([59.938196, 30.364809], {
-          hintContent: 'Санкт-Петербург, Озерной пер., д. 14',
-          balloonContent: 'Санкт-Петербург, Озерной пер., д. 14'
+          hintContent: address,
+          balloonContent: address
         }, {
           iconLayout: 'default#image',
-          iconImageHref: 'img/map-pointer.png',
+          iconImageHref: '/local/templates/main/img/map-pointer.png',
           iconImageSize: [61, 90],
           iconImageOffset: [0, -90]
         });
